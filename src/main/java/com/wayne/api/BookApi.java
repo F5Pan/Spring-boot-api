7eb43f4310c82094ab30c090c228beffab41bff3
F5Pan <storyj14@gmail.com>
2020-05-15 00:20:08 +0800

    程式新增: put方法如果不回傳整包物件 也可以做更新的方法 請見CustomBeanUtils類中的寫法

--- src/main/java/com/wayne/api/BookApi.java
+++ src/main/java/com/wayne/api/BookApi.java
@@ -1,8 +1,13 @@
 package com.wayne.api;
 
+import java.beans.PropertyDescriptor;
 import java.util.List;
 
+import javax.persistence.Convert;
+
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,59 +16,59 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wayne.domain.Book;
+import com.wayne.dto.BookDTO;
 import com.wayne.service.BookService;
-import com.wayne.service.BookServiceImpl;
 
 @RestController
 @RequestMapping("/api/v1")
 public class BookApi {
 
-//	@Autowired
-//	private BookService bookService;
-
 	@Autowired
-	private BookServiceImpl bookServiceImpl;
+	private BookService bookService;
+
 
 	@GetMapping("/books")
 	public ResponseEntity<?> listAllBooks() {
-		List<Book> books = bookServiceImpl.findAllBooks();
+		List<Book> books = bookService.findAllBooks();
 		return new ResponseEntity<List<Book>>(books, HttpStatus.OK);
 	}
 
 	@GetMapping("/books/{id}")
 	public ResponseEntity<?> getBook(@PathVariable Long id) {
-		Book book = bookServiceImpl.getBookById(id);
+		Book book = bookService.getBookById(id);
 		return new ResponseEntity<Book>(book, HttpStatus.OK);
 
 	}
 
 	@PostMapping("/books")
-	public ResponseEntity<?> saveBook(Book book) {
-		Book book1 = bookServiceImpl.saveBook(book);
+	public ResponseEntity<?> saveBook(@RequestBody Book book) {
+		Book book1 = bookService.saveBook(book);
 		return new ResponseEntity<Book>(book1, HttpStatus.CREATED);
 	}
 
 	@PutMapping("/books/{id}")
-	public ResponseEntity<?> updateBook(@PathVariable Long id, Book book) {
-		Book currentBook = bookServiceImpl.getBookById(id);
-		BeanUtils.copyProperties(book, currentBook);
-		Book book2 = bookServiceImpl.updateBook(currentBook);
-		return new ResponseEntity<Book>(book2, HttpStatus.OK);
+	public ResponseEntity<?> updateBook(@PathVariable Long id, @RequestBody BookDTO bookDTO) {
+		 Book currentBook = bookService.getBookById(id);
+	        bookDTO.convertToBook(currentBook);
+	        Book book1 = bookService.updateBook(currentBook);
+	        return new ResponseEntity<Object>(book1, HttpStatus.OK);
 	}
+	
 
 	@DeleteMapping("/books/{id}")
 	public ResponseEntity<?> deleteBook(@PathVariable Long id) {
-		bookServiceImpl.deleteBookById(id);
+		bookService.deleteBookById(id);
 		return new ResponseEntity<Object>(HttpStatus.NO_CONTENT);
 	}
 
 	@DeleteMapping("/books/")
 	public ResponseEntity<?> deleteAllBooks() {
-		bookServiceImpl.deleteAllBook();
+		bookService.deleteAllBook();
 		return new ResponseEntity<Object>(HttpStatus.NO_CONTENT);
 	}
 }
---
+++ src/main/java/com/wayne/dto/Convert.java
@@ -0,0 +1,9 @@
+package com.wayne.dto;
+
+
+public interface Convert<S,T> {
+
+    T convert(S s,T t);
+
+    T convert(S s);
+}
